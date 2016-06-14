fb3abea0443891996120f4a35bda7a0e4dc9d5f6
ROOOO <ql.wang.gd@gmail.com>
2016-06-14 17:56:29 +0800

    distinguish windows and mac

---
+++ Files/Getting Start/main.cpp
@@ -0,0 +1,27 @@
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdlib.h>
+
+int main(){
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	
+	system("Pause");
+	return 0;
+}
