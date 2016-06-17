f26a1efbb41e81eab18bbcf4d6b508a4948d925d
ROOOO <ql.wang.gd@gmail.com>
2016-06-17 13:37:58 +0800

    Fix setvs.py bugs. Now you can run it many time.

--- Tools/Win/SetVS.py
+++ Tools/Win/SetVS.py
@@ -14,28 +14,43 @@ class SetVS:
     file = open(self.projPath, 'r+')
     self.file = file.read()
     file.close()
-  def writeFile(self):
+  def writeLibAndInclude(self):
     libAndInclude = """
-  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
+    <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
     <IncludePath>D:\Projects\OpenGL\LearningOpenGL\Libs\Includes;$(IncludePath)</IncludePath>
     <LibraryPath>D:\Projects\OpenGL\LearningOpenGL\Libs\</LibraryPath>
-  </PropertyGroup>
-    """
-    link = """
-  <AdditionalDependencies>opengl32.lib;glfw3.lib;glew32s.lib;%(AdditionalDependencies)</AdditionalDependencies>
-  </Link>
-    """
-    self.readFile()
+  </PropertyGroup>"""
     pattern = re.compile(r'<PropertyGroup />', re.S)
-    rst = re.sub(pattern, libAndInclude, self.file)
-    pattern = re.compile(r'(<Link>.*?</GenerateDebugInformation>\n\s+)</Link>', re.S)
-    linkPrefix = re.findall(pattern, rst)[0]
-    link = linkPrefix + link
-    pattern = re.compile(r'<Link>.*?</GenerateDebugInformation>\n\s+(</Link>)', re.S)
-    rst = re.sub(pattern, link, rst)
+    find = re.findall(pattern, self.file)
+    if find == []:
+      pattern = re.compile(r'(<PropertyGroup Label="UserMacros" />\n).*?(<PropertyGroup.*?</PropertyGroup>)', re.S)
+      rst = re.sub(pattern, r'\1' + libAndInclude, self.file)
+    else:
+      rst = re.sub(pattern, libAndInclude, self.file)
+
     file = open(self.projPath, 'w+')
     file.write(rst)
     file.close()
+
+  def writeLink(self):
+    link = """
+      <AdditionalDependencies>glew32s.lib;glfw3.lib;opengl32.lib;kernel32.lib;user32.lib;gdi32.lib;winspool.lib;comdlg32.lib;advapi32.lib;shell32.lib;ole32.lib;oleaut32.lib;uuid.lib;odbc32.lib;odbccp32.lib;%(AdditionalDependencies)</AdditionalDependencies>
+    </Link>"""
+    pattern = re.compile(r'(<Link>.*?</GenerateDebugInformation>)(\n\s+)</Link>', re.S)
+    find = re.findall(pattern, self.file)
+    if find == []:
+      pattern = re.compile(r'(<Link>.*?</GenerateDebugInformation>\n\s+)(<AdditionalDependencies>.*?</AdditionalDependencies>.*?</Link>)', re.S)
+      rst = re.sub(pattern, r'\1' + link, self.file)
+    else:
+      rst = re.sub(pattern, r'\1' + link, self.file)
+    file = open(self.projPath, 'w+')
+    file.write(rst)
+    file.close()
+
+  def writeFile(self):
+    self.readFile()
+    self.writeLibAndInclude()
+    # self.writeLink()
 
 def MainFunc(path):
   vs = SetVS(path)
