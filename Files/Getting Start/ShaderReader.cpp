610f6a8c48d9045f8abfbb61e7b7fdbecaebbdb0
ROOOO <ql.wang.gd@gmail.com>
2016-06-18 14:26:13 +0800

    add shader reader

---
+++ Files/Getting Start/ShaderReader.cpp
@@ -0,0 +1,9 @@
+//
+//  ShaderReader.cpp
+//  Mac
+//
+//  Created by King on 16/6/18.
+//  Copyright © 2016年 King. All rights reserved.
+//
+
+#include "ShaderReader.hpp"
---
+++ Files/Getting Start/ShaderReader.hpp
@@ -0,0 +1,14 @@
+//
+//  ShaderReader.hpp
+//  Mac
+//
+//  Created by King on 16/6/18.
+//  Copyright © 2016年 King. All rights reserved.
+//
+
+#ifndef ShaderReader_hpp
+#define ShaderReader_hpp
+
+#include <stdio.h>
+
+#endif /* ShaderReader_hpp */
