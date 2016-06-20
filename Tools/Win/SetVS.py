80dbc98fa78f309a4e9f999e9ecce3abe742c9a4
ROOOO <ql.wang.gd@gmail.com>
2016-06-20 14:25:08 +0800

    fix setvs.py bugs.

--- Tools/Win/SetVS.py
+++ Tools/Win/SetVS.py
@@ -17,13 +17,13 @@ class SetVS:
   def writeLibAndInclude(self):
     libAndInclude = """
     <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
-    <IncludePath>D:\Projects\OpenGL\LearningOpenGL\Libs\Includes;$(IncludePath)</IncludePath>
-    <LibraryPath>D:\Projects\OpenGL\LearningOpenGL\Libs\;$(IncludePath)</LibraryPath>
+    <IncludePath>D:\Projects\OpenGL\LearningOpenGL\Libs\Includes;D:\Projects\OpenGL\LearningOpenGL\Files\Classes;$(IncludePath)</IncludePath>
+    <LibraryPath>D:\Projects\OpenGL\LearningOpenGL\Libs\;$(LibraryPath)</LibraryPath>
   </PropertyGroup>"""
     pattern = re.compile(r'<PropertyGroup />', re.S)
     find = re.findall(pattern, self.file)
     if find == []:
-      pattern = re.compile(r'(<PropertyGroup Label="UserMacros" />\n).*?(<PropertyGroup.*?</PropertyGroup>)', re.S)
+      pattern = re.compile(r'(<PropertyGroup Label="UserMacros" />).*?(<PropertyGroup.*?</PropertyGroup>)', re.S)
       rst = re.sub(pattern, r'\1' + libAndInclude, self.file)
     else:
       rst = re.sub(pattern, libAndInclude, self.file)
@@ -33,8 +33,7 @@ class SetVS:
     file.close()
 
   def writeLink(self):
-    link = """
-      <AdditionalDependencies>glew32s.lib;glfw3.lib;opengl32.lib;kernel32.lib;user32.lib;gdi32.lib;winspool.lib;comdlg32.lib;advapi32.lib;shell32.lib;ole32.lib;oleaut32.lib;uuid.lib;odbc32.lib;odbccp32.lib;%(AdditionalDependencies)</AdditionalDependencies>
+    link = """<AdditionalDependencies>SOIL.lib;glew32s.lib;glfw3.lib;opengl32.lib;%(AdditionalDependencies)</AdditionalDependencies>
     </Link>"""
     pattern = re.compile(r'(<Link>.*?</GenerateDebugInformation>)(\n\s+)</Link>', re.S)
     find = re.findall(pattern, self.file)
@@ -50,7 +49,8 @@ class SetVS:
   def writeFile(self):
     self.readFile()
     self.writeLibAndInclude()
-    # self.writeLink()
+    self.readFile()
+    self.writeLink()
 
 def MainFunc(path):
   vs = SetVS(path)
