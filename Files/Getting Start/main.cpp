163dd467a8f5265a9515eae059675a356551f9ec
ROOOO <ql.wang.gd@gmail.com>
2016-06-20 14:27:08 +0800

    end of Shader, Getting Start.

--- Files/Getting Start/main.cpp
+++ Files/Getting Start/main.cpp
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include "ShaderReader.hpp"
-#include <SOIL.h>
+#include <SOIL/SOIL.h>
 
 // test1  VAO and VBO
 // test2  VAO, VBO, EBO
@@ -22,176 +22,15 @@
 
 #define test 9
 
+#ifdef __APPLE__
+const GLchar* ShadersFolder = "/Users/king/git/LearningOpenGL/Files/Getting Start/";
+const GLchar* ResourcesFolder = "/Users/king/git/LearningOpenGL/Files/Getting Start/";
+#else
+string ShadersFolder = "D:\\Projects\\OpenGL\\LearningOpenGL\\Files\\Getting Start\\Shaders\\";
+string ResourcesFolder = "D:\\Projects\\OpenGL\\LearningOpenGL\\Files\\Getting Start\\Resources\\";
+#endif
+
 #include <iostream>
-
-// GLEW
-#define GLEW_STATIC
-#include <GL/glew.h>
-
-// GLFW
-#include <GLFW/glfw3.h>
-
-// Other Libs
-#include <SOIL.h>
-
-// Other includes
-//#include "Shader.h"
-
-
-// Function prototypes
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
-
-// Window dimensions
-const GLuint WIDTH = 800, HEIGHT = 600;
-
-// The MAIN function, from here we start the application and run the game loop
-int maina()
-{
-  // Init GLFW
-  glfwInit();
-  // Set all the required options for GLFW
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  
-  // Create a GLFWwindow object that we can use for GLFW's functions
-  GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
-  glfwMakeContextCurrent(window);
-  
-  // Set the required callback functions
-  glfwSetKeyCallback(window, key_callback);
-  
-  // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
-  glewExperimental = GL_TRUE;
-  // Initialize GLEW to setup the OpenGL Function pointers
-  glewInit();
-  
-  // Define the viewport dimensions
-  glViewport(0, 0, WIDTH, HEIGHT);
-  
-  
-  // Build and compile our shader program
-//  Shader ourShader("path/to/shaders/textures.vs", "path/to/shaders/textures.frag");
-  ShaderReader ourShader("/Users/king/git/LearningOpenGL/Files/Getting Start/shader_test9.vs", "/Users/king/git/LearningOpenGL/Files/Getting Start/shader_test9.frag");
-  
-  // Set up vertex data (and buffer(s)) and attribute pointers
-  GLfloat vertices[] = {
-    // Positions          // Colors           // Texture Coords
-    0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
-    0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
-    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
-    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left
-  };
-  GLuint indices[] = {  // Note that we start from 0!
-    0, 1, 3, // First Triangle
-    1, 2, 3  // Second Triangle
-  };
-  GLuint VBO, VAO, EBO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-  glGenBuffers(1, &EBO);
-  
-  glBindVertexArray(VAO);
-  
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-  
-  // Position attribute
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
-  glEnableVertexAttribArray(0);
-  // Color attribute
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-  glEnableVertexAttribArray(1);
-  // TexCoord attribute
-  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
-  glEnableVertexAttribArray(2);
-  
-  glBindVertexArray(0); // Unbind VAO
-  
-  
-  // Load and create a texture
-  GLuint texture1;
-  GLuint texture2;
-  // ====================
-  // Texture 1
-  // ====================
-  glGenTextures(1, &texture1);
-  glBindTexture(GL_TEXTURE_2D, texture1); // All upcoming GL_TEXTURE_2D operations now have effect on our texture object
-  // Set our texture parameters
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// Set texture wrapping to GL_REPEAT
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  // Set texture filtering
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  // Load, create texture and generate mipmaps
-  int width, height;
-  unsigned char* image = SOIL_load_image("/Users/king/git/LearningOpenGL/Files/Getting Start/container.jpg", &width, &height, 0, SOIL_LOAD_RGB);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-  glGenerateMipmap(GL_TEXTURE_2D);
-  SOIL_free_image_data(image);
-  glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
-  // ===================
-  // Texture 2
-  // ===================
-  glGenTextures(1, &texture2);
-  glBindTexture(GL_TEXTURE_2D, texture2);
-  // Set our texture parameters
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  // Set texture filtering
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  // Load, create texture and generate mipmaps
-  image = SOIL_load_image("/Users/king/git/LearningOpenGL/Files/Getting Start/awesomeface.png", &width, &height, 0, SOIL_LOAD_RGB);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-  glGenerateMipmap(GL_TEXTURE_2D);
-  SOIL_free_image_data(image);
-  glBindTexture(GL_TEXTURE_2D, 0);
-  
-  
-  // Game loop
-  while (!glfwWindowShouldClose(window))
-  {
-    // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
-    glfwPollEvents();
-    
-    // Render
-    // Clear the colorbuffer
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    
-    // Activate shader
-    ourShader.Use();
-    
-    // Bind Textures using texture units
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture1);
-    glUniform1i(glGetUniformLocation(ourShader.GetProgram(), "ourTexture1"), 0);
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-    glUniform1i(glGetUniformLocation(ourShader.GetProgram(), "ourTexture2"), 1);
-    
-    // Draw container
-    glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    glBindVertexArray(0);
-    
-    // Swap the screen buffers
-    glfwSwapBuffers(window);
-  }
-  // Properly de-allocate all resources once they've outlived their purpose
-  glDeleteVertexArrays(1, &VAO);
-  glDeleteBuffers(1, &VBO);
-  glDeleteBuffers(1, &EBO);
-  // Terminate GLFW, clearing any resources allocated by GLFW.
-  glfwTerminate();
-  return 0;
-}
 
 // Is called whenever a key is pressed/released via GLFW
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
@@ -340,7 +179,7 @@ int main(){
 #elif test == 8
   ShaderReader ShaderReader("/Users/king/git/LearningOpenGL/Files/Getting Start/shader_test8.vs", "/Users/king/git/LearningOpenGL/Files/Getting Start/shader_test8.frag");
 #elif test == 9
-  ShaderReader ShaderReader("/Users/king/git/LearningOpenGL/Files/Getting Start/shader_test9.vs", "/Users/king/git/LearningOpenGL/Files/Getting Start/shader_test9.frag");
+  ShaderReader ShaderReader((ShadersFolder + "shader_test9.vs").c_str(), (ShadersFolder + "shader_test9.frag").c_str());
 #endif
   
   ////////////////////////////// VAO VBO EBO //////////////////////////////
@@ -472,75 +311,33 @@ int main(){
   int texWidth[2], texHeight[2];
   // awesomeface.png
   // container.jpg
-  images[0] = SOIL_load_image("/Users/king/git/LearningOpenGL/Files/Getting Start/container.jpg", &texWidth[0], &texHeight[0], 0, SOIL_LOAD_RGB);
-  images[1] = SOIL_load_image("/Users/king/git/LearningOpenGL/Files/Getting Start/awesomeface.png", &texWidth[1], &texHeight[1], 0, SOIL_LOAD_RGB);
+  images[0] = SOIL_load_image((ResourcesFolder + "container.jpg").c_str(), &texWidth[0], &texHeight[0], 0, SOIL_LOAD_RGB);
+  images[1] = SOIL_load_image((ResourcesFolder + "awesomeface.png").c_str(), &texWidth[1], &texHeight[1], 0, SOIL_LOAD_RGB);
   
   GLuint textures[2];
   glGenTextures(2, textures);
   
-//  for (int i = 0; i < 2; i++) {
-//    if (i == 0) {
-//      image = SOIL_load_image("/Users/king/git/LearningOpenGL/Files/Getting Start/awesomeface.png", &texWidth[0], &texHeight[0], 0, SOIL_LOAD_RGB);
-//    } else {
-//      image = SOIL_load_image("/Users/king/git/LearningOpenGL/Files/Getting Start/container.jpg", &texWidth[1], &texHeight[1], 0, SOIL_LOAD_RGB);
-//    }
-//    
-//    glBindTexture(GL_TEXTURE_2D, textures[i]);
-//    
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//    
-//    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth[i], texHeight[i], 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-//    glGenerateMipmap(GL_TEXTURE_2D);
-//    
-//    SOIL_free_image_data(image);
-//    glBindTexture(GL_TEXTURE_2D, 0);
-//  }
-  
-//  GLuint texture1;
-//  GLuint texture2;
-  // ====================
-  // Texture 1
-  // ====================
-  int width, height;
-  unsigned char* image = SOIL_load_image("/Users/king/git/LearningOpenGL/Files/Getting Start/container.jpg", &width, &height, 0, SOIL_LOAD_RGB);
-//
-  glGenTextures(2, textures);
-  glBindTexture(GL_TEXTURE_2D, textures[0]); // All upcoming GL_TEXTURE_2D operations now have effect on our texture object
-  // Set our texture parameters
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// Set texture wrapping to GL_REPEAT
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  // Set texture filtering
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  // Load, create texture and generate mipmaps
-//  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, images[0]);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-  glGenerateMipmap(GL_TEXTURE_2D);
-//  SOIL_free_image_data(images[0]);
-  SOIL_free_image_data(image);
-  glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
-  // ===================
-  // Texture 2
-  // ===================
-//  glGenTextures(1, &texture2);
-  glBindTexture(GL_TEXTURE_2D, textures[1]);
-  // Set our texture parameters
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  // Set texture filtering
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  // Load, create texture and generate mipmaps
-  image = SOIL_load_image("/Users/king/git/LearningOpenGL/Files/Getting Start/awesomeface.png", &width, &height, 0, SOIL_LOAD_RGB);
-//  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, images[1]);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-  glGenerateMipmap(GL_TEXTURE_2D);
-//  SOIL_free_image_data(images[1]);
-  SOIL_free_image_data(image);
-  glBindTexture(GL_TEXTURE_2D, 0);
+  for (int i = 0; i < 2; i++) {
+    //if (i == 0) {
+    //  image = SOIL_load_image("/Users/king/git/LearningOpenGL/Files/Getting Start/awesomeface.png", &texWidth[0], &texHeight[0], 0, SOIL_LOAD_RGB);
+    //} else {
+    //  image = SOIL_load_image("/Users/king/git/LearningOpenGL/Files/Getting Start/container.jpg", &texWidth[1], &texHeight[1], 0, SOIL_LOAD_RGB);
+    //}
+    
+    glBindTexture(GL_TEXTURE_2D, textures[i]);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth[i], texHeight[i], 0, GL_RGB, GL_UNSIGNED_BYTE, images[i]);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    
+    SOIL_free_image_data(images[i]);
+    glBindTexture(GL_TEXTURE_2D, 0);
+  }
+
 #endif
   
   ////////////////////////////// main loop //////////////////////////////
--- Files/Getting Start/shader_test8.frag
+++
@@ -1,9 +0,0 @@
-#version 330 core
-in vec3 ourColor;
-in vec2 TexCoord;
-out vec4 color;
-uniform sampler2D ourTexture;
-void main() {
-  color = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0f);
-//  color = vec4(TexCoord, 0.0f, 1.0f);
-}
