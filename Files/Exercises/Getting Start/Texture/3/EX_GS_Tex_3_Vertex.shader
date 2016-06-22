a6fe74591aa7c63cdd7aceb3a2769a9bd8221810
ROOOO <ql.wang.gd@gmail.com>
2016-06-22 23:30:10 +0800

    exercise 3, some bugs will be fixed tomorrow.

--- Files/Exercises/Getting Start/Texture/3/EX_GS_Tex_3_Vertex.shader
+++ Files/Exercises/Getting Start/Texture/3/EX_GS_Tex_3_Vertex.shader
@@ -1,6 +1,5 @@
 #version 330 core
-uniform float x;
 layout (location = 0) in vec3 position;
 void main() {
-  gl_Position = vec4(position.x + x, position.yz, 1.0f);
+  gl_Position = vec4(position, 1.0f);
 }
--- Files/Exercises/Getting Start/Texture/3/main.cpp
+++ Files/Exercises/Getting Start/Texture/3/main.cpp
@@ -22,11 +22,46 @@ int main() {
   
   ShaderReader shader(Settings.CCExercisesPath(path + "EX_GS_Tex_3_Vertex.shader").c_str(), Settings.CCExercisesPath(path + "EX_GS_Tex_3_Fragment.shader").c_str());
   
+  GLfloat vertices[] = {
+    0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
+    0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+    -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+  };
+  GLint indices[] = {
+    0, 1, 2,
+    0, 2, 3,
+  };
+  GLuint VAO, VBO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+  
+  glBindVertexArray(VAO);
+  
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(vertices), (GLvoid*)0);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(vertices), (GLvoid*)(3 * sizeof(GLfloat)));
+  glEnableVertexAttribArray(1);
+  
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+  
+  glBindBuffer(GL_TEXTURE_2D, 0);
+  glBindVertexArray(0);
+  
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
     
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    shader.Use();
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
     
     glfwSwapBuffers(window);
   }
