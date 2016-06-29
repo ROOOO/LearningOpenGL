12c6603697857e9e58135fdc2ba32955c6e61aea
ROOOO <ql.wang.gd@gmail.com>
2016-06-29 23:59:34 +0800

    finished ex3 of basic lighting.

--- Files/Exercises/Lighting/Basic Lighting/2/EX_L_BL_2.fs
+++ Files/Exercises/Lighting/Basic Lighting/2/EX_L_BL_2.fs
@@ -1,7 +1,30 @@
 #version 330 core
 out vec4 color;
-//in vec3 Normal;
+
+in vec3 Normal;
+in vec3 fragPos;
+
+uniform vec3 lightColor;
+uniform vec3 objectColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+float ambientStrength = 0.1f;
+vec3 ambient = ambientStrength * objectColor;
+
+vec3 norm = normalize(Normal);
+vec3 lightDir = normalize(lightPos - fragPos);
+float diff = max(dot(lightDir, norm), 0.0f);
+vec3 diffuse = diff * objectColor;
+
+float specularStrength = 0.5f;
+vec3 reflectDir = reflect(-lightDir, norm);
+vec3 viewDir = normalize(viewPos - fragPos);
+float spec = pow(max(dot(reflectDir, viewDir), 0.0f), 32);
+vec3 specular = spec * specularStrength * objectColor;
+
+vec3 result = (ambient + diffuse + specular) * lightColor;
 
 void main() {
-  color = vec4(1.0f);
+  color = vec4(result, 1.0f);
 }
--- Files/Exercises/Lighting/Basic Lighting/2/EX_L_BL_2.vs
+++ Files/Exercises/Lighting/Basic Lighting/2/EX_L_BL_2.vs
@@ -3,6 +3,7 @@ layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 normal;
 
 out vec3 Normal;
+out vec3 fragPos;
 
 uniform mat4 modelMat;
 uniform mat4 viewMat;
@@ -10,4 +11,6 @@ uniform mat4 projMat;
 
 void main() {
   gl_Position = projMat * viewMat * modelMat * vec4(position, 1.0f);
+  Normal = mat3(transpose(inverse(modelMat))) * normal;
+  fragPos = vec3(modelMat * vec4(position, 1.0f));
 }
