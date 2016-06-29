42e62d9cfb7c063c075d820bc9d6dd976fde1d61
ROOOO <ql.wang.gd@gmail.com>
2016-06-29 15:04:35 +0800

    enhance common settings.

--- Files/Classes/CommonSettings.hpp
+++ Files/Classes/CommonSettings.hpp
@@ -14,6 +14,11 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "Camera.hpp"
+#include "ShaderReader.hpp"
 
 using namespace std;
 
--- Files/Lighting/main.cpp
+++ Files/Lighting/main.cpp
@@ -7,15 +7,7 @@
 //
 
 #include <iostream>
-#define GLEW_STATIC
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 #include "CommonSettings.hpp"
-#include "Camera.hpp"
-#include "ShaderReader.hpp"
 
 // test1 basic lighting
 
