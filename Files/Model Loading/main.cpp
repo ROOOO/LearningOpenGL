5231164739be0572055646ee62b7b04e80defc32
ROOOO <ql.wang.gd@gmail.com>
2016-07-07 23:42:20 +0800

    add assimp libs.

---
+++ Files/Model Loading/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Model Loading
+//
+//  Created by King on 16/7/7.
+//  Copyright © 2016年 King. All rights reserved.
+//
+
+#include "CommonSettings.hpp"
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
